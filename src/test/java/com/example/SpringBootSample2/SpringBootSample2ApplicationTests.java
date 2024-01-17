d481d62cc26815a4558f464f176bd59b699d4535
yumi <hinayumi0303@gmail.com>
2024-01-18 00:19:24 +0900

    初期設定

---
+++ src/test/java/com/example/SpringBootSample2/SpringBootSample2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootSample2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSample2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
